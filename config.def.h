9a4fef8bef3df9afae502b949abc861909e8a838
Josh Daniel <jd8287dan@proton.me>
2024-10-08 15:51:58 -0400

    save6

--- config.def.h
+++ config.def.h
@@ -10,6 +10,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "ZedMono Nerd Font Mono:style=Regular:size=10" };
 static const char dmenufont[]       = "ZedMono Nerd Font Mono:style=Regular:size=10";
 static const char normbgcol[]             = "#191c33";
