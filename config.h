b00700dfd36cc70d71c43e1799ca5b45165cce06
Josh Daniel <jd8287dan@proton.me>
2024-10-08 15:20:36 -0400

    save2

--- config.h
+++ config.h
@@ -3,11 +3,16 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "ZedMono Nerd Font Mono:style=Regular:size=10" };
 static const char dmenufont[]       = "ZedMono Nerd Font Mono:style=Regular:size=10";
-static const char normbgcol[]             = "#222222";
+static const char normbgcol[]             = "#191c33";
 static const char normbordercol[]         = "#444444";
 static const char normfgcol[]             = "#bbbbbb";
 static const char selfgcol[]              = "#000000";
