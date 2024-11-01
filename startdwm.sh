2f34a9e469751e00132cf2a9206af741a468dba6
Josh Daniel <jd8287dan@proton.me>
2024-10-31 23:37:52 -0400

    (dwm) added volume % to status bar

--- startdwm.sh
+++ startdwm.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-<<<<<<< HEAD
 feh --bg-scale $HOME/Pictures/ahl4bxeqdoz51.png
-=======
-feh --bg-scale /home/joshd97/Pictures/ahl4bxeqdoz51.png
->>>>>>> 3cb18f951f0eaa0f03492bb64949cb307aeefde9
 setxkbmap us
 picom --backend glx --vsync --daemon &
-while true; do xsetroot -name " 󰂎 $(cat /sys/class/power_supply/BAT0/capacity)% | $(date +"󰃭 %a %d %b %Y |  %I:%M:%S %p %Z")"; sleep 1; done &
+while true; do
+    volume=$(amixer get Master | grep -o '[0-9]*%' | head -n1 | tr -d '%')
+    xsetroot -name " 󰂎 $(cat /sys/class/power_supply/BAT0/capacity)% |  $volume% | $(date +"󰃭 %a %d %b %Y |  %I:%M:%S %p %Z  ")"; sleep 1; 
+done &
+
+pactl set-card-profile bluez_card.E8_EE_CC_A5_C3_6A a2dp-sink\n
 
 while true; do
     # Log stderror
@@ -16,5 +17,3 @@ while true; do
     # No error log
     #dwm >/dev/null 2>&1
 done
-
-
